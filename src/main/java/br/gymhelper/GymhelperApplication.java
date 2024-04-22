706d53727fcc720cdf9f3f9e060f8634dea4be01
Marcin Borkowski <marcin.d.borkowski@gmail.com>
2024-04-22 08:57:04 +0200

    Initial commit

---
+++ src/main/java/br/gymhelper/GymhelperApplication.java
@@ -0,0 +1,13 @@
+package br.gymhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymhelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymhelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gymhelper/GymhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gymhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymhelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
